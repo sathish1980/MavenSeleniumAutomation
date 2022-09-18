1393a08497d36a080b90bf0f9e2e92e7dedd7c95
sathish1980 <kumar.sathish189@gmail.com>
2022-09-18 11:09:58 +0530

    new projject creation

---
+++ src/main/java/auto/MavenSeleniumAutomation/App.java
@@ -0,0 +1,13 @@
+package auto.MavenSeleniumAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
