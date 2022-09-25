dca869b0d2ef63e876f4c64002cdbff2b2a5186e
sathish1980 <kumar.sathish189@gmail.com>
2022-09-25 11:25:55 +0530

    locators

--- src/test/java/SeleniumBasics/FirstTestcase.java
+++ src/test/java/SeleniumBasics/FirstTestcase.java
@@ -1,6 +1,8 @@
 package SeleniumBasics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -22,7 +24,7 @@ public class FirstTestcase
 		driver.manage().window().maximize();
 		}
 		driver.get("https://www.facebook.com/");
-		driver.get("https://www.gmail.com/");
+		//driver.get("https://www.gmail.com/");
 	}
 	
 	public void favorites() throws InterruptedException
@@ -32,6 +34,21 @@ public class FirstTestcase
 		driver.navigate().forward();
 		Thread.sleep(2000);
 		driver.navigate().refresh();
+	}
+	
+	public void locators()
+	{
+		//driver.findElement(By.id("email"));.sendKeys("kumar.sathish189@gmail.com");
+		WebElement username=driver.findElement(By.name("email"));
+		username.sendKeys("kumar.sathish189@gmail.com");
+		username.clear();
+		//driver.findElement(By.className("inputtext _55r1 _6luy")).sendKeys("sathish");
+		//driver.findElement(By.linkText("Forgotten password?")).click();
+		//driver.findElement(By.partialLinkText("word?")).click();
+		driver.findElement(By.cssSelector("input#email")).sendKeys("cssselector");
+		driver.findElement(By.cssSelector("input[data-testid=royal_email]")).sendKeys("3rdtype");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("xpath");
+		driver.findElement(By.xpath("//button[contains(@id,'u_0')]")).click();
 	}
 	
 	public void close()
@@ -44,7 +61,7 @@ public class FirstTestcase
 	{
 		FirstTestcase FT = new FirstTestcase();
 		FT.launch("chrome");
-		FT.favorites();
-		FT.close();
+		FT.locators();
+		//FT.close();
 	}
 }
